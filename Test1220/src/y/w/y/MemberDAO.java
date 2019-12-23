4cae3e1e06a7b3c30c096e931874ee046fc18bb9
user <user@DESKTOP-LCPNV7F>
2019-12-23 10:04:36 +0900

    MemberDAO Modify

--- Test1220/src/y/w/y/MemberDAO.java
+++ Test1220/src/y/w/y/MemberDAO.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class MemberDAO {
-	private static ArrayList<MemberDTO> db=new ArrayList<MemberDTO>();
+	private static ArrayList<MemberDTO> db =new ArrayList<MemberDTO> ();
 	public MemberDAO() {
 	}
 	public ArrayList<MemberDTO> list(){
@@ -14,21 +14,21 @@ public class MemberDAO {
 		return db.get(db.indexOf(dto));
 	}
 	public void create(MemberDTO dto) {
-		System.out.println(dto.getId()+":아이디를 생성했습니다");
+		System.out.println(dto.getId()+"아이디를 생성 합니다.");
 		db.add(dto);
-		}
+	}
 	public void update(MemberDTO dto) {
-		System.out.println(dto.getId()+":아이디를 수정했습니다");
+		System.out.println(dto.getId()+"아이디를 수정 합니다.");
 		db.set(db.indexOf(dto),dto);
 	}
 	public void delete(MemberDTO dto) {
-		System.out.println(dto.getId()+":아이디를 삭제했습니다");
+		System.out.println(dto.getId()+"아이디를 삭제 합니다.");
 		db.remove(db.indexOf(dto));
 	}
 	public boolean idCheck(String id) {
 		return db.indexOf(new MemberDTO(id))==-1?true:false;
 	}
 	public boolean idPattern(String id) {
-		return Pattern.matches("^[a-zA-Z0-9]*$", id)?true:false;
+		return Pattern.matches("^[a-zA-Z0-9]*$",id);
 	}
 }
